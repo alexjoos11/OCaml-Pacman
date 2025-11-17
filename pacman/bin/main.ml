bd62ed5e3a8e94f747e27e3764c64aa5341dccc3
Jon Snider <js3667@cornell.edu>
2025-11-16 19:40:07 -0500

    added the stuff that we added in the assignment setups

--- pacman/bin/main.ml
+++ pacman/bin/main.ml
@@ -1,1 +1,14 @@
-let () = print_endline "Hello, World!"
+open Raylib
+
+let () =
+  init_window 800 600 "Raylib Test";
+
+  while not (window_should_close ()) do
+    begin_drawing ();
+    clear_background Color.raywhite;
+    draw_text "Hello World!" 350 280 20 Color.black;
+    end_drawing ()
+  done;
+
+  close_window ();
+
