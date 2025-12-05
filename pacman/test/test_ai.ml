8a7fee469798464f6bcd80b816ad13df69912f82
Alex Joos <aaj46@cornell.edu>
2025-12-05 14:23:01 -0500

    wip of ai.test

--- pacman/test/test_ai.ml
+++ pacman/test/test_ai.ml
@@ -15,8 +15,6 @@ let assert_move msg expected f =
 (*  Tests                                                        *)
 (* ------------------------------------------------------------- *)
 
-(* making sure the wrappers work using attack. other modes should be the same
-   too *)
 let test_default_ghosts _ =
   assert_move "defaulty.attack uses standard_attack" (1, 0)
     (defaulty.attack ~x:0 ~y:0 ~pac_pos:(5, 1));
@@ -28,14 +26,12 @@ let test_default_ghosts _ =
     (pinkfaulty.attack ~x:0 ~y:0 ~pac_pos:(5, 1))
 
 let test_ai_colors _ =
-  assert_equal Color.red defaulty.color;
-  assert_equal Color.orange orangefaulty.color;
-  assert_equal Color.pink pinkfaulty.color;
-  assert_equal (Color.create 0 255 255 255) cyanfaulty.color
+  assert_equal ~msg:"defaulty should be red" Color.red defaulty.color;
 
-(* ------------------------------------------------------------- *)
-(*  Suite + main                                                 *)
-(* ------------------------------------------------------------- *)
+  assert_equal ~msg:"orangefaulty should be orange" Color.orange
+    orangefaulty.color;
+
+  assert_equal ~msg:"pinkfaulty should be pink" Color.pink pinkfaulty.color
 
 let suite =
   "ai tests"
