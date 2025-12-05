60ca8e3d02939fa7f9788ae62fe30ca6d180d04b
Kirti <kb593@cornell.edu>
2025-12-04 20:33:39 -0500

    Implement display of instructions prior to game start - Draw a gray rectangle to clearly display text - Modify renderer.ml to display the instructions - Move Press play to start label up

--- pacman/bin/renderer.ml
+++ pacman/bin/renderer.ml
@@ -233,8 +233,41 @@ let draw (w : world_view) =
   (* On-screen messages based on the phase of the game *)
   match w.state with
   | Game_state.Intro ->
-      pulsing_title "PAC-MAN" 100 60 Color.yellow;
-      blinking "Press SPACE to start" 250 30 Color.white
+      let top_box_width = 500 in
+      let top_box_height = 170 in
+      let top_box_xpos = (window_width - top_box_width) / 2 in
+      let top_box_ypos = 0 in
+      draw_rectangle top_box_xpos top_box_ypos top_box_width top_box_height
+        Color.darkgray;
+      pulsing_title "PAC-MAN" 50 60 Color.yellow;
+      let box_width = 500 in
+      let box_height = 600 in
+      let box_xpos = (window_width - box_width) / 2 in
+      let box_ypos = 170 in
+      draw_rectangle box_xpos box_ypos box_width box_height Color.darkgray;
+      draw_centered "Instructions" box_ypos 20 Color.white;
+      draw_centered "Use the arrow keys to move Pac-man around the maze."
+        (box_ypos + 30) 18 Color.black;
+      draw_centered "You begin each game with 3 lives." (box_ypos + 60) 18
+        Color.black;
+      draw_centered "If a ghost catches you, you lose a life and the"
+        (box_ypos + 90) 18 Color.black;
+      draw_centered "game ends. The ghosts will change speeds for a"
+        (box_ypos + 120) 18 Color.black;
+      draw_centered "period time with an associate timer. There are"
+        (box_ypos + 150) 18 Color.black;
+      draw_centered "power pellets that look like orange balls or cherries"
+        (box_ypos + 180) 18 Color.black;
+      draw_centered "and Pac-man will speed up as these are eaten."
+        (box_ypos + 210) 18 Color.black;
+      draw_centered "There is a period of time indicated by the state"
+        (box_ypos + 240) 18 Color.black;
+      draw_centered "label of the ghosts that Pac-man can eat the ghosts."
+        (box_ypos + 270) 18 Color.black;
+      draw_centered "At the end the final and high scores will be displayed."
+        (box_ypos + 300) 18 Color.black;
+      draw_centered "Good Luck!" (box_ypos + 330) 18 Color.black;
+      blinking "Press SPACE to start" 550 30 Color.white
   | Game_state.PacDead -> draw_centered_outline "YOU DIED!" 200 50 Color.red
   | Game_state.LevelComplete ->
       draw_centered_outline "LEVEL COMPLETE!" 200 50 Color.green
